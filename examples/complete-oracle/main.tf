f3c29279ac8670c3f9ee24a314a9b3628756888c
assareh <assareh@hashicorp.com>
2020-06-30 12:29:01 -0700

    fixed random password, and updated to most recent version of module

--- examples/complete-oracle/main.tf
+++ examples/complete-oracle/main.tf
@@ -86,27 +86,29 @@ data "aws_ami" "ubuntu" {
 #####
 module "db" {
   source  = "terraform-aws-modules/rds/aws"
-  version = "1.13.0"
+  version = "~> 2.0"
 
   identifier = "demodb-oracle"
 
   engine            = "oracle-se2"
   engine_version    = "12.1.0.2.v8"
   instance_class    = "db.t3.small"
-  allocated_storage = 10
-  storage_encrypted = false
-  license_model     = "license-included"
+  allocated_storage = 5
+
+  license_model = "license-included"
 
   # Make sure that database name is capitalized, otherwise RDS will try to recreate RDS instance every time
-  name                                = "DEMODB"
-  username                            = var.db_username
-  password                            = random_id.db_password
-  port                                = "1521"
+  name     = "DEMODB"
+  username = var.db_username
+  password = random_password.db_password.result
+  port     = "1521"
+
   iam_database_authentication_enabled = false
 
-  vpc_security_group_ids = [data.aws_security_group.default.id]
-  maintenance_window     = "Mon:00:00-Mon:03:00"
-  backup_window          = "03:00-06:00"
+  vpc_security_group_ids = [aws_security_group.oracle.id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
 
   # disable backups to create DB faster
   backup_retention_period = 0
@@ -118,24 +120,22 @@ module "db" {
   }
 
   # DB subnet group
-  subnet_ids = [aws_subnet.subnet_a.id]
+  subnet_ids = ["aws_subnet.subnet_a.id"]
 
   # DB parameter group
   family = "oracle-ee-12.1"
 
   # DB option group
-  #major_engine_version = "12.1"
+  major_engine_version = "12.1"
 
   # Snapshot name upon DB deletion
   final_snapshot_identifier = "demodb"
 
   # See here for support character sets https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.OracleCharacterSets.html
-  #character_set_name = "AL32UTF8"
-
-  # Database Deletion Protection
-  #deletion_protection = false
+  character_set_name = "AL32UTF8"
 }
 
-resource random_id "db_password" {
-  byte_length = 22
+resource random_password "db_password" {
+  length  = 20
+  special = true
 }
