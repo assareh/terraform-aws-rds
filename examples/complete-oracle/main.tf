d40663bff1effbe8a024a7090b1de58036f9faf9
assareh <assareh@hashicorp.com>
2020-07-01 14:41:02 -0700

    locking down 1521 rule

--- examples/complete-oracle/main.tf
+++ examples/complete-oracle/main.tf
@@ -252,7 +252,7 @@ resource aws_security_group "hashicat" {
     from_port   = 1521
     to_port     = 1521
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
