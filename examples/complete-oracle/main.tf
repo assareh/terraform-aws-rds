12e7634bfead781aea489af0f6054330304723e2
assareh <assareh@hashicorp.com>
2020-07-01 14:38:15 -0700

    modifying sg rules

--- examples/complete-oracle/main.tf
+++ examples/complete-oracle/main.tf
@@ -245,19 +245,12 @@ resource aws_security_group "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["76.93.151.110/32"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 1521
+    to_port     = 1521
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
