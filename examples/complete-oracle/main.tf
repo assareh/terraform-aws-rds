132a42a4291f08347b3e970f903e412802470575
assareh <assareh@hashicorp.com>
2020-07-01 12:42:50 -0700

    adding hashicat

--- examples/complete-oracle/main.tf
+++ examples/complete-oracle/main.tf
@@ -190,3 +190,98 @@ resource random_password "db_password" {
   length  = 20
   special = true
 }
+
+
+
+
+resource aws_instance "hashicat" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.oracle.key_name
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.hashicat.id
+  vpc_security_group_ids      = [aws_security_group.hashicat.id]
+
+  tags = {
+    Name = "${var.prefix}-hashicat-instance"
+  }
+}
+
+resource aws_vpc "hashicat" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc"
+  }
+}
+
+resource aws_subnet "hashicat" {
+  vpc_id     = aws_vpc.hashicat.id
+  cidr_block = var.subnet_prefix
+
+  tags = {
+    name = "${var.prefix}-subnet"
+  }
+}
+
+resource aws_security_group "hashicat" {
+  name = "${var.prefix}-security-group"
+
+  vpc_id = aws_vpc.hashicat.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.prefix}-security-group"
+  }
+}
+
+resource aws_internet_gateway "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+
+  tags = {
+    Name = "${var.prefix}-internet-gateway"
+  }
+}
+
+resource aws_route_table "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hashicat.id
+  }
+}
+
+resource aws_route_table_association "hashicat" {
+  subnet_id      = aws_subnet.hashicat.id
+  route_table_id = aws_route_table.hashicat.id
+}
